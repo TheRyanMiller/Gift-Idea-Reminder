289b4ea2e2cfd78a871eb0be641efb2e42555d10
Ryan <rmiller07@gmail.com>
2016-05-23 22:13:35 -0400

    debugging navigation menu

--- app/src/main/java/com/rtmillerprojects/giftideareminder/MainActivity.java
+++ app/src/main/java/com/rtmillerprojects/giftideareminder/MainActivity.java
@@ -1,18 +1,31 @@
 package com.rtmillerprojects.giftideareminder;
 
 import android.content.Context;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.rtmillerprojects.giftideareminder.R;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, MainListener{
 
     TabLayout tabLayout;
     ViewPager viewPager;
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    TextView headerName;
+    TextView headerNameUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
 
+        headerName = (TextView) findViewById(R.id.header_name);
+        headerNameUserName = (TextView) findViewById(R.id.header_username);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener(){
             @Override
@@ -44,6 +61,42 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.getTabAt(0).setIcon(R.drawable.calendar);
         tabLayout.getTabAt(1).setIcon(R.drawable.contacts80);
         tabLayout.getTabAt(2).setIcon(R.drawable.gift96);
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody") @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        //implement click handlers here
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private class CustomAdapter extends FragmentPagerAdapter {
@@ -72,5 +125,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    public DrawerLayout getDrawer() {
+        return drawer;
     }
 }
---
+++ app/src/main/java/com/rtmillerprojects/giftideareminder/MainListener.java
@@ -0,0 +1,11 @@
+package com.rtmillerprojects.giftideareminder;
+
+import android.support.v4.widget.DrawerLayout;
+
+/**
+ * Created by Ryan on 5/23/2016.
+ */
+public interface MainListener {
+
+    DrawerLayout getDrawer();
+}
